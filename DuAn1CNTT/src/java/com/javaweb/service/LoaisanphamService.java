244ec19e9dbbb6c7c3d1e53d5fd7c1060485062a
phuocdanh <phuocdanh2081992@gmail.com>
2016-11-26 16:53:04 +0700

    Load dữ liệu Menu

---
+++ DuAn1CNTT/src/java/com/javaweb/service/LoaisanphamService.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javaweb.service;
+
+import com.javaweb.hibernate.util.NewHibernateUtil;
+import com.javaweb.model.Loaisanpham;
+import com.javaweb.model.Sanpham;
+import java.util.ArrayList;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author PhuocDanh
+ */
+public class LoaisanphamService {
+
+    public ArrayList<Loaisanpham> GetAllLoaisanpham(int idparent) {
+        Session session = NewHibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        ArrayList<Loaisanpham> ListLoaSanPham = new ArrayList<>();
+        try {
+            tx = session.getTransaction();
+            tx.begin();
+            String strquery = "from Loaisanpham where Idparent=" + idparent;
+            Query query = session.createQuery(strquery);
+
+            ListLoaSanPham = (ArrayList<Loaisanpham>) query.list();
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            System.out.println(e.toString());
+        } finally {
+            session.close();
+        }
+        return ListLoaSanPham;
+    }
+}
